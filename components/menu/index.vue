5b1de94f299ebf3299a0e8ef5bd770347c935de7
Vincent Maret <vincent.maret@digitevent.com>
2022-04-06 02:48:04 +0200

    feat: add menu

---
+++ components/menu/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="menu">
+    <MenuItem
+      v-for="item in items"
+      :key="item.path"
+      :item="item"
+      class="mb-4"
+    />
+  </nav>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import MenuItem, { Item } from './menuItem.vue'
+
+@Component({ components: { MenuItem } })
+export default class Menu extends Vue {
+  items: Item[] = [
+    {
+      path: '/',
+      icon: 'cv',
+    },
+    {
+      path: '/jobs',
+      icon: 'job',
+    },
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+.menu {
+  height: 100vh;
+  width: 80px;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-right: 1px solid rgb(209, 209, 209);
+  padding: 2rem 0;
+}
+</style>
---
+++ components/menu/menuItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtLink :to="item.path">
+    <Icon :name="item.icon" />
+  </NuxtLink>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import Icon from '@/components/ui/icon/index.vue'
+
+export interface Item {
+  path: string
+  icon: string
+}
+
+@Component({ components: { Icon } })
+export default class MenuItem extends Vue {
+  @Prop({ required: true }) readonly item!: Item
+}
+</script>
