384226f7035ca167e99cd2f964ad1c1e15e65f0f
Vincent Maret <vincent.maret@digitevent.com>
2022-04-06 02:47:23 +0200

    feat: add icon ui component

---
+++ components/ui/icon/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <component :is="componentName" :style="iconStyle" />
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+const availableIcons = ['job', 'cv'] as const
+type AvailableIcons = typeof availableIcons[number]
+
+@Component({
+  components: {
+    job: () => import('./job.vue'),
+    cv: () => import('./cv.vue'),
+  },
+})
+export default class IconWrapper extends Vue {
+  @Prop({ required: true }) readonly name!: AvailableIcons
+  @Prop({ default: 3 }) readonly size!: number
+  @Prop({ default: 'black' }) readonly color!: string
+
+  get componentName(): AvailableIcons {
+    if (!availableIcons.includes(this.name)) {
+      throw new Error(`Icon ${this.name} is not available`)
+    }
+
+    return this.name
+  }
+
+  get iconStyle(): Record<string, string> {
+    return {
+      width: `${this.size}rem`,
+      height: `${this.size}rem`,
+      color: this.color,
+    }
+  }
+}
+</script>
