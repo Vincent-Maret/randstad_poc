2714e39bbb72d47633df71baee54db8194d35af3
Vincent Maret <vincent.maret@digitevent.com>
2022-04-06 02:47:02 +0200

    feat: add dashboard layout

---
+++ layouts/dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Menu />
+    <div class="page">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import Menu from '@/components/menu/index.vue'
+
+@Component({ components: { Menu } })
+export default class DashboardLayout extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.page {
+  width: calc(100vw - 80px);
+  margin-left: 80px;
+}
+</style>
