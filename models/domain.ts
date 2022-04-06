cef49a1a03736e07a75a0cb27e4a21d48ed548b2
Vincent Maret <vincent.maret@digitevent.com>
2022-04-06 16:19:32 +0200

    fix: missing Skill type

--- models/domain.ts
+++ models/domain.ts
@@ -3,6 +3,8 @@ import { JobId } from '@/fakeDb'
 export interface Skill {
   title: string
   uri: string
+  broaderHierarchyConcept?: Set<string>
+  broaderSkill?: Set<string>
 }
 
 export interface Score {
