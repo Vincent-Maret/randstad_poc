95310b2017a2a89e652c6c34dc9743274a30cf63
Vincent Maret <vincent.maret@digitevent.com>
2022-04-06 18:21:06 +0200

    feat: use search instead of suggest

--- services/esco.ts
+++ services/esco.ts
@@ -1,15 +1,13 @@
 import axios from 'axios'
 import { SearchResult, Type } from '@/models/esco'
 
-// https://ec.europa.eu/esco/api/resource/skill?uri=http://data.europa.eu/esco/skill/21d2f96d-35f7-4e3f-9745-c533d2dd6e97&language=fr
-
 const BASE_URL: string = 'https://ec.europa.eu/esco/api'
 
 export async function searchSkill(
   text: string,
   type: Type = 'skill'
 ): Promise<SearchResult> {
-  const res = await axios.get(`${BASE_URL}/suggest2`, {
+  const res = await axios.get(`${BASE_URL}/search`, {
     params: {
       text,
       type,
