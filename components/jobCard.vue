c1227a127ce23f8a2aafe9c7881b165077db1cf1
Vincent Maret <vincent.maret@digitevent.com>
2022-04-06 02:48:00 +0200

    feat: add job page

---
+++ components/jobCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="job-card">
+    <h3 class="title">{{ job.title }}</h3>
+    <div class="type">{{ job.type }}</div>
+    <div class="location">{{ job.location }}</div>
+    <div class="description" v-html="job.description" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import { JobOffer } from '@/fakeDb'
+
+@Component
+export default class JobCard extends Vue {
+  @Prop({ required: true }) readonly job!: JobOffer
+}
+</script>
+
+<style lang="scss" scoped>
+.job-card {
+  padding: 1rem;
+  background: rgb(226, 226, 226);
+  border-radius: 5px;
+}
+</style>
---
+++ pages/jobs.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-4">
+    <JobCard v-for="job in jobOffers" :key="job.id" :job="job" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { jobOffers, JobOffer } from '@/fakeDb'
+import JobCard from '@/components/jobCard.vue'
+
+@Component({ layout: 'dashboard', components: { JobCard } })
+export default class Jobs extends Vue {
+  jobOffers: JobOffer[] = jobOffers
+}
+</script>
