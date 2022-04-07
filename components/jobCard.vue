89aed0f6dda327ec66f712e07426d20248649594
Vincent Maret <vincent.maret@digitevent.com>
2022-04-07 20:58:42 +0200

    feat: implement match page

--- components/jobCard.vue
+++ components/jobCard.vue
@@ -1,26 +1,75 @@
 <template>
   <div class="job-card">
-    <h3 class="title">{{ job.title }}</h3>
-    <div class="type">{{ job.type }}</div>
-    <div class="location">{{ job.location }}</div>
-    <div class="description" v-html="job.description" />
+    <div class="afinity">
+      <div class="d-flex">
+        <div class="progress-bar">
+          <div class="bar" :style="barStyle" />
+        </div>
+        <div class="score">
+          {{ score }}%
+          <Icon name="heart" size="1.5" />
+        </div>
+      </div>
+    </div>
+    <div class="contents">
+      <h2 class="title">{{ job.title }}</h2>
+      <div class="type">{{ job.type }}</div>
+      <div class="location d-flex align-items-center justify-content-end">
+        <Icon name="location" size="0.8" />
+        <div class="ml-2">{{ job.location }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
 import { JobOffer } from '@/models/domain'
+import Icon from '@/components/ui/icon/index.vue'
 
-@Component
+@Component({ components: { Icon } })
 export default class JobCard extends Vue {
   @Prop({ required: true }) readonly job!: JobOffer
+  @Prop({ required: true }) readonly score!: number
+
+  get barStyle(): Record<string, string> {
+    return {
+      width: `${this.score}%`,
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .job-card {
   padding: 1rem;
-  background: rgb(226, 226, 226);
+  background: white;
   border-radius: 5px;
+  box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.1);
+}
+
+.title {
+  font-family: 'Graphik-Semibold';
+  max-width: 70%;
+}
+
+.progress-bar {
+  background: var(--pink-light);
+  width: 100%;
+  height: 2px;
+  margin-top: 8px;
+  .bar {
+    height: 2px;
+    background: var(--custom-pink);
+  }
+}
+
+.score {
+  font-family: 'Graphik-Semibold';
+  color: var(--custom-pink);
+  margin-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
--- pages/matchs.vue
+++ pages/matchs.vue
@@ -1,28 +1,58 @@
 <template>
-  <div class="p-4">
-    <JobCard v-for="job in jobOffers" :key="job.id" class="mb-4" :job="job" />
+  <div class="p-5">
+    <h1>Consulter mes <span>matchs</span></h1>
+    <JobCard
+      v-for="job in jobOffers"
+      :key="job.id"
+      class="mb-4"
+      :job="job"
+      :score="scores[job.id]"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
 import JobCard from '@/components/jobCard.vue'
-// import { getJobsMatchingScore } from '@/services/matcher'
 import { getJobOffers } from '@/services/nocodb'
-import { Score, Skill, JobOffer } from '@/models/domain'
+import { Skill, JobOffer } from '@/models/domain'
 
 @Component({ components: { JobCard } })
 export default class Matchs extends Vue {
   jobOffers: JobOffer[] = []
-  matchingScores: Score[] = []
 
-  get userSkills(): Skill[] {
-    return this.$store.state.userSkills
+  get userSkills(): Set<number> {
+    return new Set(this.$store.state.user.skills.map((s: Skill) => s.id))
+  }
+
+  get scores(): Record<number, number> {
+    const scores: Record<number, number> = {}
+
+    for (const job of this.jobOffers) {
+      scores[job.id] = this.computeScore(job)
+    }
+
+    return scores
   }
 
   async mounted(): Promise<void> {
     this.jobOffers = await getJobOffers()
-    // this.matchingScores = getJobsMatchingScore(this.userSkills, this.jobOffers)
+  }
+
+  computeScore(job: JobOffer): number {
+    const matchingSkills: number = job.skillList.filter((s) =>
+      this.userSkills.has(s.id)
+    ).length
+
+    return matchingSkills === 0
+      ? 0
+      : (matchingSkills / job.skillList.length) * 100
   }
 }
 </script>
+
+<style lang="scss" scoped>
+h1 > span {
+  color: var(--custom-pink);
+}
+</style>
