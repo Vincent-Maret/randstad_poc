1144532d2fe6875429c25eeb6c0ec215b8ddac7e
Vincent Maret <vincent.maret@digitevent.com>
2022-04-07 06:13:41 +0200

    refactor: create axios instance for nocodb

--- services/nocodb.ts
+++ services/nocodb.ts
@@ -5,35 +5,25 @@ const BASE_URL: string = 'https://randstaddb.herokuapp.com'
 const token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hcmV0dmluY2VudEBwcm90b25tYWlsLmNvbSIsImZpcnN0bmFtZSI6bnVsbCwibGFzdG5hbWUiOm51bGwsImlkIjoxLCJyb2xlcyI6InVzZXIiLCJpYXQiOjE2NDkyMzkwMDJ9.6hM1pyTIKyMdDyR6gVC7iWg21t2HFdiWDxsfBu40z6o' as const
 
+const nocoDbAxios = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'xc-auth': token,
+    'content-type': 'application/json',
+  },
+})
+
 export async function createJobOffer(
   jobData: Partial<JobOffer>
 ): Promise<void> {
-  await axios.post(
-    `${BASE_URL}/nc/randstad_3l3o/api/v1/jobOffer`,
-    {
-      ...jobData,
-      skills: { data: jobData.skills },
-    },
-    {
-      headers: {
-        'xc-auth': token,
-        'content-type': 'application/json',
-      },
-    }
-  )
+  await nocoDbAxios.post('/nc/randstad_3l3o/api/v1/jobOffer', {
+    ...jobData,
+    skills: { data: jobData.skills },
+  })
 }
 
 export async function getJobOffers(): Promise<JobOffer[]> {
-  const res = await axios.get(
-    `${BASE_URL}/nc/randstad_3l3o/api/v1/jobOffer`,
-
-    {
-      headers: {
-        'xc-auth': token,
-        'content-type': 'application/json',
-      },
-    }
-  )
+  const res = await nocoDbAxios.get('/nc/randstad_3l3o/api/v1/jobOffer')
 
   return res.data.map((jobOffer: any) => ({
     ...jobOffer,
